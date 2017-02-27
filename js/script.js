49d9f7dd6dfbd5021a603ab0cbc4006a9f9638ab
Tevin Joel Aduma <tevin74@live.com>
2017-02-27 15:06:26 +0300

    complete business logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-//business logic 
+//business logic
+functon Adventure(resort, destination, time, landmarks, experience) {
+    this.resort = resort;
+    this.destination = destination;
+    this.time =  time;
+    this.landmarks = landmarks;
+    this.experience = experience;
+}
+
+//user interface logic
