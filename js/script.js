a62c22cabae560041bfb10cf0f6553b86052150c
Tevin Joel Aduma <tevin74@live.com>
2017-02-27 15:10:43 +0300

    create new variables for the inputted data

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ functon Adventure(resort, destination, time, landmarks, experience) {
 }
 
 //user interface logic
+$(document).ready(function() {
+    $("form#new-adventure").submit(function(event) {
+        event.preventDefault();
+
+        var inputtedResort = $("input#resort").val();
+        var inputtedDestination = $("input#destination").val();
+        var inputtedTime = $("select#time").val();
+        var inputtedLandmarks = $("input#landmarks").val();
+        var inputtedExperience = $("input#experience").val();
